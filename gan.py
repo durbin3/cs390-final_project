e57a22d4ea7947ab64f30eb87be73d32ad9c4aeb
Eric Durbin <durbin3@purdue.edu>
2021-11-07 22:52:51 -0500

    Generated LowRes/HighRes Images

--- gan.py
+++ gan.py
@@ -4,8 +4,10 @@ import tensorflow.keras.losses as losses
 import tensorflow.keras.optimizers as opt
 import tensorflow.keras.models as models
 import pandas as pd
+from PIL import Image
 import numpy as np 
-
+import os
+import matplotlib.pyplot as plt
 
 def main():
     raw = getRawData()
@@ -15,11 +17,26 @@ def main():
 
 ##### Model Pipeline #####
 def getRawData():
-    pass
+    print("Loading Raw Images")
+    train_images = get_training_images()
+    test_images = get_test_images()
+    return train_images,test_images
 
 def preprocessData(raw):
-    X_train,X_test,y_train,y_test = {}
-    return (X_train,X_test,y_train,y_test)
+    x,y = raw
+    num_imgs = len(x)
+    split = .8
+    train_size = num_imgs * split
+    test_size = num_imgs * (1-split)
+    
+    return (x_train,x_test,y_train,y_test)
+
+
+def make_generator():
+    pass
+
+def make_discriminator():
+    pass
 
 def trainModel(data):
     x,y = data
@@ -52,7 +69,30 @@ def normalize_col(col):
     return (col-col.min())/(col.max()-col.min()) #max_min normalization
     return (col - col.mean())/(col.std()) # z-score normalization (better if outliers exist)
 
+def get_training_images():
+    print("\tLoading Training Images\t", end='')
+    i = 0
+    images = np.array([])
+    for image in os.listdir('low_res'):
+        if i % 100 == 0: 
+            print(".",end='')
+        np.append(images,np.array(Image.open("low_res/"+image)))
+        i +=1
+    print("\tDone")
+    return images
 
+def get_test_images():
+    print("\tLoading Test Images\t", end='')
+    i = 0
+    images = np.array([])
+    for image in os.listdir('high_res'):
+        if i % 100 == 0: 
+            print(".",end='')
+        np.append(images,np.array(Image.open("high_res/"+image)))
+        i +=1
+        
+    print("\tDone")
+    return images
 
 if __name__ == '__main__':
     main()
