a1ff3c1018bf7114d6ecdaa6a48cac89e8b29cba
danielchen09 <chen3189@purdue.edu>
2021-11-27 16:31:16 -0500

    #add tensorboard

--- srgan.py
+++ srgan.py
@@ -4,6 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # uncomment if using cpu
 from tensorflow import keras
 from tensorflow.keras.losses import MeanSquaredError, binary_crossentropy, BinaryCrossentropy
+from tensorflow.keras.metrics import BinaryAccuracy
 from tensorflow.keras.optimizers import Adam, SGD
 import tensorflow as tf
 from config import CONFIG
@@ -12,6 +13,8 @@ from datagen import DataGenerator
 import numpy as np
 from utils import *
 import tensorflow.keras.backend as K
+from tensorboard import program
+import datetime
 
 physical_devices = tf.config.list_physical_devices('GPU')
 if len(physical_devices) > 0:
@@ -49,11 +52,17 @@ def build_gan(generator, discriminator, vgg) -> Model:
     gan.compile(loss=[vgg_loss(vgg), 'binary_crossentropy'],
                 loss_weights=[CONFIG.VGG_WEIGHT, CONFIG.D_WEIGHT],
                 optimizer=Adam(learning_rate=CONFIG.LR_START))
-    gan.summary()
     return gan
 
 
 def train():
+    log_dir = 'logs/' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    writer = tf.summary.create_file_writer(log_dir, flush_millis=1000)
+    tb = program.TensorBoard()
+    tb.configure(argv=[None, '--logdir', log_dir, '--reload_interval', '1'])
+    url = tb.launch()
+    print(f"Tensorflow listening on {url}")
+
     datagen = DataGenerator(CONFIG.HR_DIR,
                             CONFIG.INPUT_SHAPE,
                             down_sample_scale=CONFIG.DOWN_SAMPLE_SCALE,
@@ -80,29 +89,34 @@ def train():
     gan = build_gan(generator, discriminator, vgg)
     predict_random_image(generator)
 
-    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.1), metrics=['accuracy'])
+    discriminator.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=0.1))
 
+    iter = 0
     for epoch in range(CONFIG.N_EPOCH):
+        print(f'Training epoch {epoch}')
         # train discriminator
         if epoch % 2 == 0:
             discriminator.trainable = True
             for step, (lr_batch, hr_batch) in enumerate(datagen_d):
                 sr_batch = generator.predict(lr_batch)
-                print(f'Training epoch {epoch} step {step}')
                 d_loss_real = discriminator.train_on_batch(hr_batch, create_noisy_labels(0, CONFIG.BATCH_SIZE_D))
                 d_loss_gen = discriminator.train_on_batch(sr_batch, create_noisy_labels(1, CONFIG.BATCH_SIZE_D))
                 d_loss = np.add(d_loss_real, d_loss_gen) / 2
-                print(f'\tdiscriminator loss: {d_loss}')
+                iter += 1
+                with writer.as_default():
+                    tf.summary.scalar('discriminator_loss', d_loss, step=iter)
             datagen_d.on_epoch_end()
             discriminator.trainable = False
         else:
             # train gan
             for step, (lr_batch, hr_batch) in enumerate(datagen):
-                print(f'Training epoch {epoch} step {step}')
                 generator.trainable = True
                 gan_loss = gan.train_on_batch(lr_batch, [hr_batch, create_noisy_labels(1, CONFIG.BATCH_SIZE)])
                 generator.trainable = False
-                print(f'\tgan loss: {gan_loss}')
+                # print(f'\tgan loss: {gan_loss}')
+                iter += 1
+                with writer.as_default():
+                    tf.summary.scalar('gan_loss', gan_loss[0], step=iter)
             datagen.on_epoch_end()
 
         if epoch % CONFIG.PREVIEW_INTERVAL == 0:
